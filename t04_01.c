d190082afab8bf1a73f3e5d7ce9ce57e6c0aaa4f
GeraldaGultom <geraldagultom@gmail.com>
2025-02-27 13:51:50 +0700

    pair 17

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,20 @@
-// NIM - Name
-// NIM - Name
+// 12S24051 - Geralda Natali Gultom
+// 12S24018 - Kelvin Yohanes Putra
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int _argv, char **_argc) {
-  // codes
-
+  char pesan[61];
+  scanf("%s", pesan);
+  
+  int panjang = strlen(pesan);
+  for (int i = 0; i < panjang; i += 3) {
+      char tigaAngka[4] = {pesan[i], pesan[i+1], pesan[i+2], '\0'};
+      int kodeASCII = atoi(tigaAngka);
+      printf("%c", kodeASCII);
+  }
+  printf("\n");
   return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,20 @@
-// NIM - Name
-// NIM - Name
+// 12S24051 - Geralda Natali Gultom
+// 12S24018 - Kelvin Yohanes Putra
 
 #include <stdio.h>
-
+#include <string.h>
 int main(int _argv, char **_argc) {
-  // codes
 
-  return 0;
+  char pesan[21];
+    
+    fgets(pesan, sizeof(pesan), stdin);
+    
+    pesan[strcspn(pesan, "\n")] = '\0';
+    
+    for (int i = 0; i < strlen(pesan); i++) {
+        printf("%03d", (unsigned char)pesan[i]);
+    }
+  
+    printf("\n");
+    return 0;
 }

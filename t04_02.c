9880c4fc0d7bd41d8c8e4fc857c67605865806ac
GeraldaGultom <geraldagultom@gmail.com>
2025-02-27 14:24:57 +0700

    pair 17 update t04_02

--- t04_02.c
+++ t04_02.c
@@ -4,17 +4,17 @@
 #include <stdio.h>
 #include <string.h>
 int main(int _argv, char **_argc) {
-
   char pesan[21];
-    
-    fgets(pesan, sizeof(pesan), stdin);
-    
-    pesan[strcspn(pesan, "\n")] = '\0';
-    
-    for (int i = 0; i < strlen(pesan); i++) {
-        printf("%03d", (unsigned char)pesan[i]);
-    }
+
+  fgets(pesan, sizeof(pesan), stdin);
   
-    printf("\n");
-    return 0;
+  pesan[strcspn(pesan, "\n")] = '\0';
+  
+  for (int i = 0; i < strlen(pesan); i++) {
+      printf("%03d", pesan[i]);
+  }
+  
+  printf("013\n");
+  
+  return 0;
 }
